417cd79de40928aea4ef902d44b2bfc38ffb10ab
Frits Stegmann <fstegmann@gmail.com>
2019-11-30 07:49:50 +0200

    add the other components

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,10 @@ import UiPill from "./ui/UiPill";
 import UiProgress from "./ui/UiProgress";
 // @ts-ignore
 import UiModal from "./ui/UiModal";
+// @ts-ignore
+import UiRadioBox from "./ui/UiRadioBox";
+// @ts-ignore
+import UiCheckBox from "./ui/UiCheckBox";
 
 export function VueUI(v: any) {
     v.component("ui-button", UiButton);
@@ -15,4 +19,6 @@ export function VueUI(v: any) {
     v.component("ui-pill", UiPill);
     v.component("ui-progress", UiProgress);
     v.component("ui-modal", UiModal);
+    v.component("ui-radio-box", UiRadioBox);
+    v.component("ui-check-box", UiCheckBox);
 }
